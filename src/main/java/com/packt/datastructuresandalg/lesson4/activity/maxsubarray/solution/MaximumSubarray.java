308b3996863f37ce9b6fbc3097088fbcb89a9cb5
Joao Azevedo <joao.c.azevedo@gmail.com>
2018-03-10 03:15:55 +0000

    Add maximum subarray solution

---
+++ src/main/java/com/packt/datastructuresandalg/lesson4/activity/maxsubarray/solution/MaximumSubarray.java
@@ -0,0 +1,44 @@
+package com.packt.datastructuresandalg.lesson4.activity.maxsubarray.solution;
+
+public class MaximumSubarray {
+    public int maxSubarrayCross(int[] a, int l, int m, int h) {
+        int leftSum = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int i = m; i >= l; i--) {
+            sum += a[i];
+            if (sum > leftSum)
+                leftSum = sum;
+        }
+        int rightSum = Integer.MIN_VALUE;
+        sum = 0;
+        for (int i = m + 1; i <= h; i++) {
+            sum += a[i];
+            if (sum > rightSum)
+                rightSum = sum;
+        }
+        return leftSum + rightSum;
+    }
+
+    public int maxSubarrayAux(int[] a, int l, int h) {
+        if (l == h)
+            return a[l];
+        else {
+            int m = (l + h) / 2;
+            int bl = maxSubarrayAux(a, l, m);
+            int br = maxSubarrayAux(a, m + 1, h);
+            int bc = maxSubarrayCross(a, l, m, h);
+            int best = Math.max(Math.max(bl, br), bc);
+            return best;
+        }
+    }
+
+    public int maxSubarray(int[] a) {
+        return maxSubarrayAux(a, 0, a.length - 1);
+    }
+
+    public static void main(String[] args) {
+        int[] a = {13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7};
+        MaximumSubarray maxSubarray = new MaximumSubarray();
+        System.out.println("Maximum subarray = " + maxSubarray.maxSubarray(a));
+    }
+}
