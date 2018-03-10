9a51b46caa6a8acad018c127bde9cef2e461fd42
Joao Azevedo <joao.c.azevedo@gmail.com>
2018-03-10 17:41:31 +0000

    Add solution for CoinChange

---
+++ src/main/java/com/packt/datastructuresandalg/lesson4/activity/coinchange/solution/CoinChange.java
@@ -0,0 +1,23 @@
+package com.packt.datastructuresandalg.lesson4.activity.coinchange.solution;
+
+public class CoinChange {
+    public int ways(int N, int[] coins) {
+        int[][] dp = new int[N + 1][coins.length + 1];
+        for (int j = 0; j <= coins.length; j++)
+            dp[0][j] = 1;
+        for (int j = 1; j <= coins.length; j++) {
+            for (int i = 1; i <= N; i++) {
+                dp[i][j] = dp[i][j - 1];
+                if (i - coins[j - 1] >= 0)
+                    dp[i][j] += dp[i - coins[j - 1]][j];
+            }
+        }
+        return dp[N][coins.length];
+    }
+
+    public static void main(String[] args) {
+        int[] coins = {1, 2, 3};
+        CoinChange cc = new CoinChange();
+        System.out.println(cc.ways(4, coins));
+    }
+}
