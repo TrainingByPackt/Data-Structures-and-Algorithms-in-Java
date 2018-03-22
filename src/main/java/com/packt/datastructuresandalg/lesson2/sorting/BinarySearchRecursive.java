c6c62769376db8da93db5ae2d61411c328e83f75
cutajarj2 <james.cutajar@blip.pt>
2018-03-22 17:39:08 +0000

    Lesson 2 fixing typo on class name

--- src/main/java/com/packt/datastructuresandalg/lesson2/sorting/BinarySearchRecursive.java
+++ src/main/java/com/packt/datastructuresandalg/lesson2/sorting/BinarySearchRecursive.java
@@ -10,7 +10,8 @@ public class BinarySearchRecursive {
         if (start <= end) {
             int mid = (end - start) / 2 + start;
             if (sortedNumbers[mid] == x) return true;
-            if (sortedNumbers[mid] > x) return binarySearch(x, sortedNumbers, start, mid - 1);
+            if (sortedNumbers[mid] > x)
+                return binarySearch(x, sortedNumbers, start, mid - 1);
             return binarySearch(x, sortedNumbers, mid + 1, end);
         }
         return false;
