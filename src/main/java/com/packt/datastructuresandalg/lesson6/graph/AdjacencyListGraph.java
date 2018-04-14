b30a44c8faca19e692ecc4052dc19505001e7482
Joao Azevedo <joao.c.azevedo@gmail.com>
2018-04-15 00:29:23 +0100

    Fix adjacency-list representation

--- src/main/java/com/packt/datastructuresandalg/lesson6/graph/AdjacencyListGraph.java
+++ src/main/java/com/packt/datastructuresandalg/lesson6/graph/AdjacencyListGraph.java
@@ -7,14 +7,29 @@ public class AdjacencyListGraph {
 
     public AdjacencyListGraph(int nodes) {
         this.adj = new ArrayList[nodes];
+        for (int i = 0; i < nodes; i++)
+            this.adj[i] = new ArrayList<>();
     }
 
     public void addEdge(int u, int v) {
         adj[u].add(v);
     }
 
+    @Override
+    public String toString() {
+        String res = "";
+        for (int i = 0; i < adj.length; i++) {
+            res += (i + ":");
+            for (int j = 0; j < adj[i].size(); j++)
+                res += (" " + adj[i].get(j));
+            if (i + 1 < adj.length)
+                res += "\n";
+        }
+        return res;
+    }
+
     public static void main(String [] args) {
-        AdjacencyListGraph g = new AdjacencyListGraph(5);
+        AdjacencyListGraph g = new AdjacencyListGraph(6);
         g.addEdge(0, 1);
         g.addEdge(0, 3);
         g.addEdge(1, 4);
@@ -23,5 +38,6 @@ public class AdjacencyListGraph {
         g.addEdge(3, 1);
         g.addEdge(4, 3);
         g.addEdge(5, 5);
+        System.out.println(g);
     }
 }
