22cde9e35dc27ccfc3c5b6d9133b275bf5e1cce2
G Ajeet <gajeet11000@gmail.com>
2019-09-30 21:53:20 +0530

    Update SelectionSort.java

--- src/main/java/com/packt/datastructuresandalg/lesson2/activity/selectionsort/SelectionSort.java
+++ src/main/java/com/packt/datastructuresandalg/lesson2/activity/selectionsort/SelectionSort.java
@@ -3,6 +3,17 @@ package com.packt.datastructuresandalg.lesson2.activity.selectionsort;
 public class SelectionSort {
 
     public void sort(int[] array) {
-
+        int small_index, temp=0;
+        for(int i=0  ; i<array.length ; i++)
+        {
+            small_index=i;
+            for(int j=i+1 ; j<array.length ; j++)
+            if(array[j]<array[small_index])
+            small_index=j;
+            
+            temp=array[i];
+            array[i]=array[small_index];
+            array[small_index]=temp;
+        }
     }
 }
